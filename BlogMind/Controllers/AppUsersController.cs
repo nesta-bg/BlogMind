5512bbcf26afe288e861e845e63d843d52f510a5
nenadnesta <nenadnesta@gmail.com>
2020-05-25 18:15:12 +0200

    AppUsersController - Building API.

---
+++ BlogMind/Controllers/AppUsersController.cs
@@ -0,0 +1,29 @@
+﻿using BlogMind.Models;
+using BlogMind.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogMind.Controllers
+{
+    [Route("api/[controller]")]
+    public class AppUsersController : Controller
+    {
+        private readonly BlogDbContext context;
+
+        public AppUsersController(BlogDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await context.AppUsers
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .ToListAsync();
+        }
+    }
+}
--- BlogMind/Startup.cs
+++ BlogMind/Startup.cs
@@ -32,7 +32,11 @@ namespace BlogMind
 
             services.AddDefaultIdentity<AppUser>().AddEntityFrameworkStores<BlogDbContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(
+                    options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
