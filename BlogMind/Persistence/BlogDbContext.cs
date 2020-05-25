6652f611342a8e4fb751428544672e7179917f86
nenadnesta <nenadnesta@gmail.com>
2020-05-25 11:47:54 +0200

    Creating DbContext.

---
+++ BlogMind/Persistence/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogMind.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMind.Persistence
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
