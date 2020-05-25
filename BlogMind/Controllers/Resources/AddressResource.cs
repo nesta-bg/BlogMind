efc126b2c5b20792e0f74c4ecf999eb6ac5de58e
nenadnesta <nenadnesta@gmail.com>
2020-05-25 18:24:51 +0200

    AppUser and Address - Creating Resources.

---
+++ BlogMind/Controllers/Resources/AddressResource.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogMind.Controllers.Resources
+{
+    public class AddressResource
+    {
+        public int Id { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ BlogMind/Controllers/Resources/AppUserResource.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogMind.Controllers.Resources
+{
+    public class AppUserResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public AddressResource Address { get; set; }
+    }
+}
