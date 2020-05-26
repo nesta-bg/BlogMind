c31244d8cd9f31a9b93390c9805dc2fcf11011cf
nenadnesta <nenadnesta@gmail.com>
2020-05-26 11:40:23 +0200

    Adding Navbar Component.

--- BlogMind/ClientApp/src/app/app.component.ts
+++ BlogMind/ClientApp/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-navbar></app-navbar>
+  `
 })
 export class AppComponent {
-  title = 'ClientApp';
+
 }
