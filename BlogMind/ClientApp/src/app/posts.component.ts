62047ea056c7b425f8fa53610625a4983cbb2e89
nenadnesta <nenadnesta@gmail.com>
2020-05-29 20:33:49 +0200

    PostsComponent - View Post Details.

--- BlogMind/ClientApp/src/app/posts.component.ts
+++ BlogMind/ClientApp/src/app/posts.component.ts
@@ -3,11 +3,13 @@ import { Post } from './post';
 import { PostService } from './post.service';
 
 @Component({
-  templateUrl: './posts.component.html'
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
   posts: Post[] = [];
   isLoading = true;
+  currentPost: Post = null;
 
   constructor(private postService: PostService) { }
 
@@ -18,6 +20,10 @@ export class PostsComponent implements OnInit {
         () => null,
         () => { this.isLoading = false; }
       );
+  }
+
+  select(post) {
+    this.currentPost = post;
   }
 }
 
