08440dd9ac487c6774ae269ac2b96a8e5457df47
nenadnesta <nenadnesta@gmail.com>
2020-06-13 21:32:24 +0200

    VotesController - AddUserVote, DeleteUserVote.

--- BlogMind/Controllers/VotesController.cs
+++ BlogMind/Controllers/VotesController.cs
@@ -1,4 +1,5 @@
-﻿using BlogMind.Persistence;
+﻿using BlogMind.Models;
+using BlogMind.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -43,6 +44,52 @@ namespace BlogMind.Controllers
             var count = context.Votes.Where(v => v.PostId == postId && v.AppUserId != userId).Sum(v => v.Mark);
 
             return Ok(count);
+        }
+
+
+        [HttpPost("{postId}/{userId}/{mark}")]
+        public async Task<IActionResult> AddUserVote(int postId, string userId, int mark)
+        {
+            var post = await context.Posts.SingleOrDefaultAsync(p => p.Id == postId);
+            var user = await context.AppUsers.SingleOrDefaultAsync(u => u.Id == userId);
+
+            if (post == null || user == null)
+                return NotFound();
+
+            var vote = await context.Votes.SingleOrDefaultAsync(v => v.PostId == postId && v.AppUserId == userId);
+
+            if (vote != null)
+            {
+                return StatusCode(409, "This User has already voted this post.");
+            }
+
+            var newVote = new Vote
+            {
+                PostId = postId,
+                AppUserId = userId,
+                Mark = mark
+            };
+
+            context.Votes.Add(newVote);
+            await context.SaveChangesAsync();
+
+            return Ok(newVote.PostId);
+        }
+
+        [HttpDelete("{postId}/{userId}")]
+        public async Task<IActionResult> DeleteUserVote(int postId, string userId)
+        {
+            var post = await context.Posts.SingleOrDefaultAsync(p => p.Id == postId);
+            var user = await context.AppUsers.SingleOrDefaultAsync(u => u.Id == userId);
+            var vote = await context.Votes.SingleOrDefaultAsync(v => v.PostId == postId && v.AppUserId == userId);
+
+            if (post == null || user == null || vote == null)
+                return NotFound();
+
+            context.Votes.Remove(vote);
+            await context.SaveChangesAsync();
+
+            return Ok(postId);
         }
     }
 }
